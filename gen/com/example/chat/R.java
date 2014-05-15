10f06d9bddfbeff491e65e65fd2887334bf92566
feliciaf <feliciaf730@172.17.145.239>
2014-05-15 12:17:11 -0400

    Nice

--- gen/com/example/chat/R.java
+++ gen/com/example/chat/R.java
@@ -995,7 +995,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050042;
+        public static final int action_settings=0x7f050043;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
@@ -1007,6 +1007,7 @@ containing a value of this type.
         public static final int disableHome=0x7f050008;
         public static final int dropdown=0x7f05000f;
         public static final int editText1=0x7f05003d;
+        public static final int editText2=0x7f050042;
         public static final int editText3=0x7f050040;
         public static final int edit_query=0x7f050030;
         public static final int end=0x7f050013;
@@ -1103,7 +1104,8 @@ containing a value of this type.
         public static final int abc_search_view=0x7f030016;
         public static final int activity_chat=0x7f030017;
         public static final int activity_chat2=0x7f030018;
-        public static final int support_simple_spinner_dropdown_item=0x7f030019;
+        public static final int gps=0x7f030019;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001a;
     }
     public static final class menu {
         public static final int chat=0x7f0c0000;
@@ -1153,6 +1155,7 @@ containing a value of this type.
         public static final int app_name=0x7f0a000d;
         public static final int hello_world=0x7f0a000e;
         public static final int ichat=0x7f0a0011;
+        public static final int location=0x7f0a0016;
         public static final int login_=0x7f0a0014;
         public static final int register=0x7f0a0015;
         public static final int send=0x7f0a0010;
--- src/com/example/chat/Location_Gps.java
+++ src/com/example/chat/Location_Gps.java
@@ -1,5 +1,73 @@
 package com.example.chat;
 
-public class Location_Gps {
+import android.app.Activity;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+public class Location_Gps extends Activity
+{
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.gps);
+
+      /* Use the LocationManager class to obtain GPS locations */
+      LocationManager mlocManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+
+      LocationListener mlocListener = new MyLocationListener();
+      mlocManager.requestLocationUpdates( LocationManager.GPS_PROVIDER, 0, 0, mlocListener);
+    }
+
+    /* Class My Location Listener */
+    public class MyLocationListener implements LocationListener
+    {
+
+      @Override
+      public void onLocationChanged(Location loc)
+      {
+
+    	 /* Button messageButton = (Button) findViewById(R.id.button1);
+			messageButton.setOnClickListener(new View.OnClickListener(){
+
+				public void onClick(View v) {
+					Toast.makeText(Chat.this, "Logged in!", Toast.LENGTH_LONG).show();
+    	  */
+    	  
+        loc.getLatitude();
+        loc.getLongitude();
+
+        String Text = "My current location is: " +
+        "Latitud = " + loc.getLatitude() +
+        "Longitud = " + loc.getLongitude();
+
+        Toast.makeText( getApplicationContext(), Text, Toast.LENGTH_SHORT).show();
+      }
+
+      @Override
+      public void onProviderDisabled(String provider)
+      {
+        Toast.makeText( getApplicationContext(), "Gps Disabled", Toast.LENGTH_SHORT ).show();
+      }
+
+      @Override
+      public void onProviderEnabled(String provider)
+      {
+        Toast.makeText( getApplicationContext(), "Gps Enabled", Toast.LENGTH_SHORT).show();
+      }
+
+      @Override
+      public void onStatusChanged(String provider, int status, Bundle extras)
+      {
+
+      }
+    }
 }
